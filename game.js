15c54223cbee938225a49569c537ec986f8ca666
Daniel <113702174+runconis@users.noreply.github.com>
2023-02-27 11:45:09 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,23 @@
 function getComputerChoice() {
     let choice = ['Rock', 'Paper', 'Scissors'];
     let randomChoice = choice[Math.floor(Math.random() * choice.length)];
-    console.log(randomChoice);
+    return randomChoice;
 }
 
-getComputerChoice()
+function singleRound(playerSelection, computerSelection) {
+    if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        return 'You lose! Paper beats Rock';
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        return 'You win! Rock beats Scissors';
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return 'You lose! Scissors beats Paper';
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        return 'You win! Paper beats Rock';
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return 'You lose! Rock beats Scissors';
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        return 'You win! Scissors beats Paper';
+    } else if (playerSelection == computerSelection) {
+        return 'Draw';
+    }
+}
